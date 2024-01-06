b43f6f08033cebedccd6982bf7daf67e59c3780e
lamaeee <147073131+lamaeee@users.noreply.github.com>
2024-01-06 16:30:29 +0800

    Update Requisition.cs
    asmc mas

--- RentalManagement/Models/Requisition.cs
+++ RentalManagement/Models/Requisition.cs
@@ -1,4 +1,4 @@
-﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace RentalManagement.Models
@@ -13,31 +13,32 @@ namespace RentalManagement.Models
         [Required(ErrorMessage = "Requisition Type is required.")]
         public string Requisition_Type { get; set; }
 
-
-        public DateTime Requistition_CreatedAt { get; set; }
-
-
-        [StringLength(100, MinimumLength = 2, ErrorMessage = "Requisition Status characters must be more than 2 and maximum of 100 characters.")]
-        [Required(ErrorMessage = "Requisition Status is required.")]
-        public string Requisition_Status { get; set; }
-
-        [StringLength(300, MinimumLength = 2, ErrorMessage = "Requisition Status characters must be more than 2 and maximum of 300 characters.")]
-        public string? Requisition_StatusRemarks { get; set; }
-
-
-        [StringLength(300, MinimumLength = 10, ErrorMessage = "Requisition Description must be more than 10 characters & maximum of 300 characters.")]
-        public string Requisition_Status_Remarks { get; set; }
+        public string Requisition_Remarks { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime Requisition_CreatedAt { get; set; }
 
 
         [DataType(DataType.Date)]
         public DateTime Requisition_DueDate { get; set; }
 
 
+
+        [StringLength(100, MinimumLength = 2, ErrorMessage = "Requisition Status characters must be more than 2 and maximum of 100 characters.")]
+
+        public string Requisition_Status { get; set; }
+
+        [StringLength(300, MinimumLength = 2, ErrorMessage = "Requisition Status characters must be more than 2 and maximum of 300 characters.")]
+        public string? Requisition_StatusRemarks { get; set; }
+
+
         [ForeignKey("Tenant")]
-        public int? TenantId { get; set; }
+        public int TenantId { get; set; }
 
         public virtual Tenant? Tenant { get; set; }
-        public List<RequisitionItem>? RequisitionItems { get; set; }
-        public List<RequisitionService>? RequisitionServices { get; set; }
+        public virtual ICollection<RequisitionItem> RequisitionItems { get; set; }
+
+        public virtual ICollection<RequisitionService> RequisitionServices { get; set; }
+
+
     }
 }
