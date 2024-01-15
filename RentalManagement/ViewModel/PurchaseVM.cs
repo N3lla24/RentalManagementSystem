6d42ea343f785193c08d9c559abddce96bd1f2e5
lamaeee <147073131+lamaeee@users.noreply.github.com>
2024-01-16 04:26:13 +0900

    Add files via upload

---
+++ RentalManagement/ViewModel/PurchaseVM.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using RentalManagement.Models;
+
+namespace RentalManagement.ViewModel
+{
+    public class PurchaseVM
+    {
+
+        public Requisition? Requisitions { get; set; }
+        public PurchaseOrder? PurchaseOrder { get; set; }
+        public IEnumerable<PurchaseItem>? PurchaseItems { get; set; }
+        public IEnumerable<PurchaseService>? PurchaseServices { get; set; }
+        public IEnumerable<Supplier>? Suppliers { get; set; }
+        public int supplierId { get; set; }
+
+
+    }
+}
